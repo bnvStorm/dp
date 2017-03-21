46c4cdb740c993884b36414a3351dbb2046802f0
Nik Burya <nikzecu@gmail.com>
2017-03-21 16:35:53 +0600

    conection upgarde

--- includes/constants.php
+++ includes/constants.php
@@ -1,9 +1,13 @@
 <?php
 
 // Database Constants
-define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "surf");
-
+//define("DB_SERVER", "localhost");
+//define("DB_USER", "root");
+//define("DB_PASS", "");
+//define("DB_NAME", "surf");
+$host = 'localhost';
+$db   = 'test';
+$user = 'root';
+$pass = '';
+$charset = 'utf8';
 ?>
